eccb79b2ee128d58fb4ccd59becedbea7e2dea74
Ruslan Akhmetshin <ruslan.akhmetshin94@gmail.com>
2021-02-28 18:19:25 -0500

    added tasks

---
+++ bestSum.js
@@ -0,0 +1,32 @@
+//find SHORTEST combination of arr elements for sum up into targetSum (if there is a tie - return any combination)
+//targetSum = 7
+//arr = [3, 5, 4, 7]
+
+//answer: 7
+
+const bestSum = (targetSum, numbers, memo = {}) => {
+  if (targetSum in memo) return memo[targetSum];
+  if (targetSum === 0) return [];
+  if (targetSum < 0) return null;
+  let shortestCombination = null;
+
+  for (let num of numbers) {
+    const remainder = targetSum - num;
+    const remainderCombination = bestSum(remainder, numbers, memo);
+
+    if (remainderCombination !== null) {
+      const combination = [...remainderCombination, num];
+      if (
+        shortestCombination === null ||
+        combination.length < shortestCombination.length
+      ) {
+        shortestCombination = combination;
+      }
+    }
+  }
+
+  memo[targetSum] = shortestCombination;
+  return shortestCombination;
+};
+
+console.log(bestSum(100, [3, 4, 5, 25]));
